8cc690b2960386206af5f25bdaaa897426a6ef3d
Andrew Lee <lee_andrew@college.harvard.edu>
2018-01-28 15:26:19 +0000

    sdf

--- flaskr/flaskr/application.py
+++ flaskr/flaskr/application.py
@@ -1,7 +1,23 @@
-from flask import Flask, redirect, render_template, request, g
+from flask import Flask, redirect, jsonify, render_template, request, g, session
+from flask_session import Session
+from tempfile import mkdtemp
 import sqlite3
 
 app = Flask(__name__)
+
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Database
 DATABASE = 'database.db'
@@ -39,6 +55,18 @@ def initdb_command():
     init_db()
     print('Initialized the database.')
 
+@app.route("/logout")
+def logout():
+    session.clear()
+    return render_template("/")
+
+@app.route("/login")
+def login():
+    print("\n\n" + request.args.get("id"))
+    session["user_id"] = request.args.get("id")
+    print(session["user_id"])
+    return render_template("/")
+
 # Routes
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -53,16 +81,18 @@ def index():
 @app.route("/addCourses", methods=['GET', 'POST'])
 def addCourses():
     if request.method == "POST":
-        name = request.form.get('username')
+        print("hello")
+        name = request.form.get('name')
         if not name:
             return redirect("/addCourses")
         courses = request.form.get('courses')
         if not courses:
             return redirect("/addCourses")
-        query_db("INSERT INTO students (name) VALUES (?)", [name])
-        studentId = query_db("SELECT id FROM students WHERE name=?", [name])
-        for course in classes.replace(" ", "").split(","):
-            query_db("INSERT INTO courses (title, student) VALUES (?, ?)", [course, studentId])
+        query_db("insert into students (name) values (?)", [name])
+        studentId = query_db("select id from students where name=?", [name])
+        print(studentId[0][0])
+        for course in courses.replace(" ", "").split(","):
+            query_db("insert into courses (title, student) values (?, ?)", [course, int(studentId[0][0])])
         print(query_db("SELECT * FROM students"))
         print(query_db("SELECT * FROM courses"))
         return redirect("/")
